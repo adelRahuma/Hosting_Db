93843d9f9de0dda6a844b83ba59b3c9d0f938177
AdelRahuma <adel_bugbug@yahoo.com>
2023-02-01 15:05:54 +0000

    Try-to-host

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,19 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 // ...
-const config = ENV === 'production'? {
+const config =
+  ENV === "production"
+    ? {
         connectionString: process.env.DATABASE_URL,
         max: 2,
-      } : {};
-require('dotenv').config({
+      }
+    : {};
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const { app } = require("./app.js");
-const PORT = process.env.PORT || 9090;
-//console.log(app.listen());
+const PORT = process.env.PORT || 3000;
+//console.log('Time: %d', Date.now())
 app.listen(PORT, function (err) {
   if (err) console.log(err);
   console.log("Server listening on PORT", PORT);
