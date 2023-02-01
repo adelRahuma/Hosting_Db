e310f4f53fbca40a657fb3c8e782cf38d5882d0f
AdelRahuma <adel_bugbug@yahoo.com>
2023-02-01 15:59:56 +0000

    test

--- listen.js
+++ listen.js
@@ -1,7 +1,4 @@
 const { app } = require("./app.js");
-const PORT = process.env.PORT || 3000;
+const { PORT = 9090 } = process.env;
 //console.log('Time: %d', Date.now())
-app.listen(PORT, function (err) {
-  if (err) console.log(err);
-  console.log("Server listening on PORT", PORT);
-});
+app.listen(PORT, () => console.log("Server listening on PORT", PORT));
